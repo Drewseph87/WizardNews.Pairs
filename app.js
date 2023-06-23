c9877c603c101df359493a7c97952173438ba8f8
Drew Harris <drewseph87@gmail.com>
2023-06-22 19:08:36 -0600

    Floyd and I Finalized Wizard News Project with time bonus

--- app.js
+++ app.js
@@ -1,9 +1,105 @@
 const express = require("express");
+const morgan = require("morgan");
+const postBank = require("./postBank");
+const timeAgo = require("node-time-ago");
+
 const app = express();
+app.use(express.static("public"));
+app.use(morgan("dev"));
 
-app.get("/", (req, res) => res.send("Hello World!"));
+// app.get("/", (req, res) => res.send("Hello World!"));
 
-const PORT = 1337;
+app.get("/", (req, res) => {
+  const posts = postBank.list();
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts
+        .map(
+          (post) => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            <a href="/posts/${post.id}">${post.title}</a>
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${timeAgo(post.date)}
+          </small>
+        </div>`
+        )
+        .join("")}
+    </div>
+  </body>
+</html>`;
+
+  res.send(html);
+});
+
+app.get("/posts/:id", (req, res) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+
+  // if (!post.id) {
+  //   // If the post wasn't found, just throw an error
+  //   throw new Error("Not Found");
+  // } else
+  if (!post.id) {
+    // If the post wasn't found, set the HTTP status to 404 and send Not Found HTML
+    res.status(404);
+    const html = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Wizard News</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+      <header><img src="/logo.png"/>Wizard News</header>
+      <div class="not-found">
+        <p>404: Page Not Found</p>
+      </div>
+    </body>
+    </html>`;
+    res.send(html);
+  } else {
+    // ... Otherwise, send the regular post detail HTML
+    res.send(`<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header>
+      <img src="/logo.png"/>Wizard News</header>
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            ${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <p>
+            <span class="news-content">${post.content}. </span>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${timeAgo(post.date)}
+          </small>
+        </div>
+    </div>
+  </body>
+</html>`);
+  }
+});
+
+const { PORT = 1337 } = process.env;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
